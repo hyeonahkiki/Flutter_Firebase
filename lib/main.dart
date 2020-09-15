5bc6548e82406088269a4df339cd0b801130a4a2
hakong <hakong@mindslab.ai>
2020-09-15 14:36:05 +0900

    connect google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,175 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(MaterialApp(
+    title: 'Google sign in',
+    home: HomePage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return Scaffold(
+      appBar: AppBar(title: Text('Firebase')),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text("google sign in test"),
+            subtitle: Text('plugin'),
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>GoogleSignInDemo()));
+            },
+          )
+        ].map((child){
+          return Card(
+            child: child,
+          );
+        }).toList(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+GoogleSignInDemoState pageState;
 
-  final String title;
+GoogleSignIn _googleSignIn = GoogleSignIn(scopes: <String>[
+  'email',
+  'https://www.googleapis.com/auth/contacts.readonly'
+]);
 
+class GoogleSignInDemo extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  GoogleSignInDemoState createState() {
+    pageState = GoogleSignInDemoState();
+    return pageState;
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class GoogleSignInDemoState extends State<GoogleSignInDemo> {
+  GoogleSignInAccount _cureentUser;
+  String _contactText;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  void initState(){
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
+      setState(() {
+        _cureentUser = account;
+      });
+      if (_cureentUser != null){
+        _handleGetContact();
+      }
+      _googleSignIn.signInSilently();
     });
+  }
+
+  Future<void>_handleGetContact()async{
+    setState(() {
+      _contactText = "Loading contact info......";
+    });
+    final http.Response response = await http.get(
+        'https://people.googleapis.com/v1/people/me/connections'
+            '?requestMask.includeField=person.names',
+      headers: await _cureentUser.authHeaders);
+
+    if(response.statusCode != 200){
+      setState(() {
+        _contactText = '${response.statusCode}';
+      });
+      print("${response.statusCode}" + '랑' +'${response.body}');
+      return;
+    }
+
+    final Map<String, dynamic> data = json.decode(response.body);
+    final String nameContact = _pickFirstNameContact(data);
+    setState(() {
+      if(nameContact != null){
+      _contactText = "${nameContact}";
+      }else{
+        _contactText = "No contact to display";
+      }
+    });
+  }
+
+  String _pickFirstNameContact(Map<String, dynamic> data) {
+    final List<dynamic> connections = data['conntections'];
+    final Map<String, dynamic> contact = connections?.firstWhere(
+            (dynamic contact) => contact['names'] != null,
+        orElse: ()=> null);
+    if (contact != null){
+      final Map<String, dynamic> name = contact['names'].firstWhere(
+          (dynamic name)=> name['displayName'] != null,
+        orElse: ()=> null
+      );
+      if (name != null){
+        return name['displayName'];
+      }
+    }
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      appBar: AppBar(title: Text('test'),),
+      body: ConstrainedBox(
+        constraints: const BoxConstraints.expand(),
+        child: _buildBody(),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget _buildBody(){
+    if(_cureentUser != null){
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          ListTile(
+            leading: GoogleUserCircleAvatar(
+              identity: _cureentUser,
+            ),
+            title: Text(_cureentUser.displayName ?? ""),
+            subtitle: Text(_cureentUser.email ?? ""),
+          ),
+          const Text("signed in successfully"),
+          Text(_contactText??""),
+          RaisedButton(
+            child: const Text('sign out'),
+            onPressed: _handleSignout,
+          ),
+          RaisedButton(
+            child: const Text('refresh'),
+            onPressed: _handleGetContact,
+          )
+        ],
+      );
+    }else{
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text('you are currentlu sign in'),
+          RaisedButton(
+            child :Text('sign in'),
+            onPressed: _handleSignIn,
+          )
+        ],
+      );
+    }
+  }
+
+  Future<void> _handleSignIn() async{
+    try{
+      await _googleSignIn.signIn();
+    }catch(error){
+      print(error);
+    }
+  }
+
+  Future<void> _handleSignout() async{
+    _googleSignIn.disconnect();
+  }
 }
+
